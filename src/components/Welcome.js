fbab35ebb72689f88fc150acf94e7cd4b2e89d01
MercedesTello <mtelloespana@gmail.com>
2022-09-26 09:52:48 -0500

    modificación

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -8,64 +8,52 @@ export const Welcome = () => {
   const header = document.createElement('header');
   header.classList.add('header');
 
-  const titleOfHeader = document.createElement('p');
-  titleOfHeader.textContent = 'Taquiero Much';
-  
-  const figureTaco = document.createElement('figure');
-  figureTaco.classList = 'figureTaco';
-  const imageTaco = document.createElement('img');
-  imageTaco.setAttribute('src', './image/taquito.png');
-  imageTaco.setAttribute('class', 'imageTaco');
-  imageTaco.setAttribute('alt', 'Imagen de un taco');
-  figureTaco.appendChild(imageTaco);
+  const imageLogo = document.createElement('img');
+  imageLogo.setAttribute('src', './image/logotaco.png');
+  imageLogo.setAttribute('class', 'imageLogo');
+  imageLogo.setAttribute('alt', 'Imagen de un taco');
 
-  const figureHeart = document.createElement('figure');
-  figureHeart.classList = 'figureHeart';
-  const imageHeart = document.createElement('img');
-  imageHeart.setAttribute('src', './image/heart.png');
-  imageHeart.setAttribute('class', 'imageHeart');
-  imageHeart.setAttribute('alt', 'Imagen de un corazón');
-  figureHeart.appendChild(imageHeart);
-
-  header.append(titleOfHeader, figureTaco, figureHeart);
+  header.append(imageLogo);
 
   //  Main of Welcome
   const mainWelcome = document.createElement('main');
   mainWelcome.setAttribute('class', 'mainWelcome');
 
+  const description = document.createElement('p');
+  description.classList.add('description');
+  description.textContent = '¿Buscas los mejores lugares de la ciudad para comer deliciosos tacos? Pues aquí encontrarás recomendaciones de lugares magníficos y podrás compartir tus tips para poder convertirte en un taquero profesional de “La calle a tu cocina”.';
+
+  const buttonSignWithGoogle = document.createElement('button');
+  buttonSignWithGoogle.classList.add('buttonSignWithGoogle');
+
+  const orSelect = document.createElement('img');
+  orSelect.setAttribute('src', './image/or.png');
+  orSelect.setAttribute('class', 'orSelect');
+  orSelect.setAttribute('alt', 'or');
+
+  const buttonLogin = document.createElement('button');
+  buttonLogin.textContent = 'Iniciar Sesión';
+  buttonLogin.classList.add('buttonLogin');
+
   const ask = document.createElement('p');
   ask.classList.add('ask');
   ask.textContent = '¿No tienes cuenta?';
-  
 
-  // const buttonSignWithGoogle = document.createElement('button');
-  // buttonSignWithGoogle.textContent = 'Iniciar sesión con Google';
-
-  // const buttonLogin = document.createElement('button');
-  //  buttonLogin.textContent = 'Iniciar Sesión';
-
-  // buttonLogin.addEventListener('click', () => {
-  //   onNavigate('/login');
-  // });
+  buttonLogin.addEventListener('click', () => {
+    onNavigate('/login');
+  });
 
   const buttonRegister = document.createElement('button');
   buttonRegister.textContent = 'Regístrate';
   buttonRegister.classList = 'buttonRegister';
 
-  mainWelcome.append(ask, buttonRegister);
+  mainWelcome.append(description, buttonSignWithGoogle, orSelect, buttonLogin, ask, buttonRegister);
 
   buttonRegister.addEventListener('click', () => {
     onNavigate('/register');
   });
 
-  // footer of Welcome
-  const footerWelcome = document.createElement('footer');
-  footerWelcome.classList.add('footer');
-  footerWelcome.textContent = 'Al registrarte, aceptas los Términos de servicio y la Política de privacidad, incluida la política de Uso de Cookie';
-  
-  
-
-  sectionWelcome.append(header, mainWelcome, footerWelcome);
+  sectionWelcome.append(header, mainWelcome);
 
   return sectionWelcome;
 };
