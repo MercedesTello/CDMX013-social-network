f29f94c1f3f211b8077c79688467550ca0b24a53
MercedesTello <mtelloespana@gmail.com>
2022-09-26 09:48:43 -0500

    agregar botón

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,22 +4,17 @@ export const Login = () => {
   const div = document.createElement('div');
   const title = document.createElement('h2');
   const buttonStartSession = document.createElement('button');
-  const buttonBack = document.createElement('button');
   const inputEmail = document.createElement('input');
   const inputPassword = document.createElement('input');
 
   buttonStartSession.textContent = 'Iniciar Sesión';
-  buttonBack.textContent = 'Regresar';
   title.textContent = 'Inicia Sesión';
 
   buttonStartSession.addEventListener('click', () => {
     onNavigate('/wall');
   });
-  buttonBack.addEventListener('click', () => {
-    onNavigate('/');
-  });
 
-  div.append(title, inputEmail, inputPassword, buttonStartSession, buttonBack);
+  div.append(title, inputEmail, inputPassword, buttonStartSession);
 
   return div;
 };
