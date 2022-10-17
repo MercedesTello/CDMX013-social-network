f99f78c615f8ae8c3a46abb88fcc0e37dfc87352
MercedesTello <mtelloespana@gmail.com>
2022-10-17 15:25:27 -0500

    hasta el editado

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,16 @@
 import { onNavigate } from '../main.js';
-import { logOut } from '../lib/auth.js';
-import { save, data } from '../lib/store.js';
+import { logOut, auth } from '../lib/auth.js';
+import {
+  save, onGetPost, data, deletePost, getPost, updatePost,
+} from '../lib/store.js';
+
+let editStatus = false;
 
 export const Home = () => {
   const sectionHome = document.createElement('section');
   sectionHome.setAttribute('class', 'sectionHome');
 
-  // Header of register
+  // Header of home
   const header = document.createElement('header');
   header.classList.add('header');
 
@@ -25,49 +29,90 @@ export const Home = () => {
   post.classList.add('post');
   post.placeholder = '¿Tienes alguna recomendación?';
 
-  const publications = document.createElement('div');
-  publications.classList.add('post-container');
-
-  const read = document.createElement('div');
-  read.classList.add('container-post');
-  read.type = 'p';
-
-  const buttonDiv = document.createElement('div');
-  buttonDiv.classList.add('container-buttons');
-
-  const buttonEdit = document.createElement('button');
-  buttonEdit.classList.add('edit');
-
-  const buttonDelete = document.createElement('button');
-  buttonDelete.classList.add('delete');
-  buttonDelete.type = 'image';
-
-  const tacoLike = document.createElement('button');
-  tacoLike.classList.add('tacoLike');
-
-  buttonDiv.append(tacoLike, buttonEdit, buttonDelete);
-
-  publications.append(read, buttonDiv);
-
   const upLoadPost = document.createElement('button');
   upLoadPost.classList.add('upLoadPost');
   upLoadPost.type = 'submit';
 
-  // crear una variable(data) que contenga toda la info de tu colección post de firebase con snapshot
-  // crear función que recorra el array data y que muestre el post, aquí traigo el div de
-  upLoadPost.addEventListener('click', async () => {
-    save({ post: post.value }).then(() => {
-      post.value = '';
-    })
-      .catch((error) => {
+  upLoadPost.addEventListener('click', () => {
+    if (!editStatus) {
+      save({
+        post: post.value,
+        date: new Date(),
+        author: auth.currentUser.uid,
+
+      }).then(() => {
+        post.value = '';
+      })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      updatePost(post.id, { post: post.value }).then((result) => {
+      }).catch((error) => {
         console.log(error);
       });
+      post.value = '';
+    }
+  }); // aquí cierra el enviar
+  // div de publicaciones
 
-    const querySnapshot = await data();
+  const newPost = (cont, item) => {
+    const publications = document.createElement('div');
+    publications.classList.add('post-container');
+
+    const read = document.createElement('div');
+    read.classList.add('container-post');
+    read.type = 'p';
+
+    const userComment = document.createElement('h3');
+    userComment.classList.add('user');
+
+    const buttonDiv = document.createElement('div');
+    buttonDiv.classList.add('container-buttons');
+
+    const buttonEdit = document.createElement('button');
+    buttonEdit.classList.add('edit');
+    buttonEdit.dataset.id = item;
+
+    const buttonDelete = document.createElement('button');
+    buttonDelete.classList.add('delete');
+    buttonDelete.dataset.id = item;
+
+    const tacoLike = document.createElement('button');
+    tacoLike.classList.add('tacoLike');
+
+    buttonDelete.addEventListener('click', (e) => {
+      deletePost(e.target.dataset.id);
+    });
+
+    buttonEdit.addEventListener('click', async (e) => {
+      const doc = await getPost(e.target.dataset.id);
+      const task = doc.data();
+      post.value = task.post;
+      post.id = doc.id;
+      editStatus = true; // para saber si está guardando o editando
+    });
+
+    buttonDiv.append(tacoLike, buttonEdit, buttonDelete);
+
+    publications.append(read, buttonDiv);
+    read.append(userComment, cont.post);
+    mainHome.append(publications);
+  };
+
+  onGetPost((querySnapshot) => {
+    // tenemos que vaciar el contenedor de los posts
+    document.querySelectorAll('.post-container').forEach((postContainer) => {
+      postContainer.remove();
+    });
 
     querySnapshot.forEach((doc) => {
       const task = doc.data();
-      read.append(task.post);
+      const postContent = task.post;
+
+      const docId = doc.id;
+
+      newPost(task, docId);
     });
   });
 
@@ -79,12 +124,12 @@ export const Home = () => {
       onNavigate('/');
       // Sign-out successful.
     })
-      .catch((error) => {
-      // An error happened.
+      .catch(() => {
+        // An error happened.
       });
   });
 
-  mainHome.append(buttonLogOut, post, upLoadPost, publications);
+  mainHome.append(buttonLogOut, post, upLoadPost);
 
   sectionHome.append(header, mainHome);
 
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -7,8 +7,15 @@ import {
 } from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js';
 import { app } from './config.js';
 
-const auth = getAuth(app);
+export const auth = getAuth(app);
+export const user = auth.currentUser;
+if (user !== null) {
+  // The user object has basic properties such as display name, email, etc.
+  const displayName = user.displayName;
+  const email = user.email;
 
+  const uid = user.uid;
+}
 // eslint-disable-next-line max-len
 export const createUser = (email, password) => (createUserWithEmailAndPassword(auth, email, password));
 export const logUser = (email, password) => (signInWithEmailAndPassword(auth, email, password));
